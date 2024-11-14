c68b7d02f2a61ad17341c57c10f226229de28631
yongsu kwon <yskwon@meatbox.co.kr>
2024-11-14 23:33:49 +0900

    FIX: merge conflict

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
-<script setup>
-import { ref } from 'vue';
-import Header from '@/components/HeaderLayout.vue';
-import AddTodo from '@/components/AddTodo.vue';
-import AddInput from '@/components/AddInput.vue';
-import AddTodoBtn from '@/components/AddTodoBtn.vue';
-
-const todoList = ref([]);
-const handleUpdateTodoList = (item) => {
-    todoList.value.push(item);
-};
-
-const addTodoActive = ref(false);
-</script>
-
 <template>
     <!-- 라우터 -->
     <div class="container">
         <Header></Header>
         <div class="wrap">
+
+            <hr>
             <div class="checkbox-wrap">
                 <add-todo
                     v-for="(todo, index) in todoList"
@@ -28,14 +15,50 @@ const addTodoActive = ref(false);
             </div>
         </div>
         <AddTodoBtn @click="addTodoActive = !addTodoActive"></AddTodoBtn>
-        <AddInput
+        <!-- <AddInput
             :class="{ active: addTodoActive, unactive: !addTodoActive }"
             @update:addTodo="handleUpdateTodoList"
-        ></AddInput>
+        ></AddInput> -->
+
+        <BottomSheet :activeStatus="addTodoActive">
+            <FormInputWithLabel
+                placeholder="일단 입력하고 생각하기"
+                @update="handleUpdateTodoList"
+            />
+        </BottomSheet>
     </div>
 </template>
 
-<style lang="scss">
+<script setup>
+import { ref } from 'vue';
+import Header from '@/components/common/HeaderLayout.vue';
+import AddTodo from '@/components/AddTodo.vue';
+// import AddInput from '@/components/AddInput.vue';
+import AddTodoBtn from '@/components/AddTodoBtn.vue';
+import BottomSheet from '@/components/common/BottomSheet.vue';
+
+import FormInputWithLabel from '@/components/form/FormInputWithLabel.vue';
+import { usePKManager } from '@/composables/usePKManager';
+
+const addTodoActive = ref(false);
+const todoList = ref([]);
+
+const { incrementPK, getPKValue } = usePKManager();
+const handleUpdateTodoList = (todoValue) => {
+    incrementPK();
+
+    const todoItem = {
+        id: `todo${getPKValue()}`,
+        todoValue
+    };
+
+    todoList.value = [...todoList.value, todoItem];
+};
+
+</script>
+
+
+<style lang="scss" scoped>
 // @import './assets/css/_variables.scss';
 .container {
     position: relative;
