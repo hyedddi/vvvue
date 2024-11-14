5d014bb1fd25c865d049a4d9ccbde74d600528da
yongsu kwon <yskwon@meatbox.co.kr>
2024-11-14 23:32:11 +0900

    FEAT: implement common components

---
+++ src/components/common/BottomSheet.vue
@@ -0,0 +1,47 @@
+<template>
+    <div
+        class="btmsht-pop"
+        :class="isActive"
+    >
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    activeStatus: {
+        type: Boolean,
+        default: false,
+    }
+});
+
+const isActive = computed(() => {
+    return props.activeStatus ? 'active' : 'inactive';
+});
+</script>
+
+<style lang="scss" scoped>
+.btmsht-pop {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    max-width: 400px;
+    height: 200px;
+    background: #fff;
+    border: 1px solid #ccc;
+    border-style: dashed;
+    border-radius: 20px 20px 0 0;
+    transition: transform 0.5s ease-in-out;
+    z-index: 100;
+}
+
+.btmsht-pop.inactive {
+    transform: translateY(100%);
+}
+
+.btmsht-pop.active {
+    transform: translateY(0);
+}
+</style>
