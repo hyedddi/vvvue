99fd98cef867748a011cd85b716c83976245128f
yongsu kwon <yskwon@meatbox.co.kr>
2024-11-14 23:32:11 +0900

    FEAT: implement common components

---
+++ src/components/common/HeaderLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <!-- TODO: title 을 h 태그가 아니라 p 태그를 사용 이유 -->
+        <p>다 울었니? 이제 할 일을 하자</p>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/css/_variables.scss';
+
+header {
+    height: 60px;
+    color: $color-333;
+    border-bottom: 1px solid $color-eee;
+    border-style: dashed;
+    text-align: center;
+
+    p {
+        line-height: 60px;
+        font-size: 2.4rem;
+        font-weight: $font-w-semibold;
+    }
+}
+</style>
