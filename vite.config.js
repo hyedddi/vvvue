e3db1b53bc80a9460970d103a30335e95720ede2
DevFrog92 <yongsu1481@gmail.com>
2024-11-16 19:00:07 +0900

    CHORE: update css preprocessor option to scss

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
-    css: {
+    scss: {
         preprocessorOptions: {
             scss: {
                 additionalData: '@import "./src/assets/scss/style";'
