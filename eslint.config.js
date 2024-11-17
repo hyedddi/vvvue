11b3cb597ab53cb62656b41848d4334fb84d3303
DevFrog92 <yongsu1481@gmail.com>
2024-11-17 17:13:07 +0900

    CHORE: setup eslint & prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,51 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginVue from "eslint-plugin-vue";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+import pluginPrettier from 'eslint-plugin-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,vue}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
+    {
+        files: ['**/*.{js,mjs,cjs,vue}'],
+        plugins: {
+            prettier: pluginPrettier, // 추가
+        },
+        rules: {
+            'no-console': 'off',
+            'vue/html-self-closing': [
+                'warn',
+                {
+                    html: {
+                        void: 'always',
+                        normal: 'never',
+                        component: 'always',
+                    },
+                    svg: 'always',
+                    math: 'always',
+                },
+            ],
+            'prettier/prettier': [
+                'error',
+                {
+                    singleQuote: true,
+                    semi: true,
+                    useTabs: false,
+                    tabWidth: 4,
+                    trailingComma: 'all',
+                    printWidth: 100,
+                    bracketSpacing: true,
+                    arrowParens: 'always',
+                    endOfLine: 'auto',
+                    bracketSameLine: false,
+                    singleAttributePerLine: true,
+                    htmlWhitespaceSensitivity: 'css',
+                },
+            ],
+        },
+    },
+    {
+        languageOptions: { globals: globals.browser },
+    },
+    pluginJs.configs.recommended,
+    ...pluginVue.configs['flat/essential'],
 ];
