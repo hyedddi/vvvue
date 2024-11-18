5b9a12f787fb6c5688ba4b698518cb81fe153b5b
iamhyedddi <iamhyedddi@gmail.com>
2024-11-18 20:44:32 +0900

    CHORE: setup eslint & prettier

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,43 @@ import pluginVue from "eslint-plugin-vue";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,vue}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
+    {
+        files: ['**/*.{js,mjs,cjs,vue}'],
+        plugins: {
+            prettier: pluginPrettier, // 추가
+        },
+        rules: {
+            'no-console': 'off',
+            'vue/html-self-closing': [
+                'warn',
+                {
+                    html: {
+                        void: 'always',
+                        normal: 'never',
+                        component: 'always',
+                    },
+                    svg: 'always',
+                    math: 'always',
+                },
+            ],
+            'prettier/prettier': [
+                'error',
+                {
+                    singleQuote: true,
+                    semi: true,
+                    useTabs: false,
+                    tabWidth: 4,
+                    trailingComma: 'all',
+                    printWidth: 100,
+                    bracketSpacing: true,
+                    arrowParens: 'always',
+                    endOfLine: 'auto',
+                    bracketSameLine: false,
+                    singleAttributePerLine: true,
+                    htmlWhitespaceSensitivity: 'css',
+                },
+            ],
+        },
+    },
+    ...
 ];
